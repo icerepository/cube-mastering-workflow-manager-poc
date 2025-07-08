5f10cf4de635a72676ce6eabcf311d7649b5c43f
Martin Chuka Chinaka <martin.chinaka@iceservices.com>
2025-07-08 16:29:01 +0200

    initial commit

---
+++ src/main/java/com/iceservices/poc/workflowmgr/CubeMasteringWorkflowManagerPocApplication.java
@@ -0,0 +1,13 @@
+package com.iceservices.poc.workflowmgr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CubeMasteringWorkflowManagerPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CubeMasteringWorkflowManagerPocApplication.class, args);
+	}
+
+}
