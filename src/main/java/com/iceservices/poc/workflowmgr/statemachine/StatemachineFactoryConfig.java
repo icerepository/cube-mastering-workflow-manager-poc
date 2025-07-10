89fc27b2c515d67d8ed2133bc33a08bdb404cd88
Martin Chuka Chinaka <martin.chinaka@iceservices.com>
2025-07-10 09:05:06 +0200

    simple config to handle event

--- src/main/java/com/iceservices/poc/workflowmgr/statemachine/StatemachineFactoryConfig.java
+++ src/main/java/com/iceservices/poc/workflowmgr/statemachine/StatemachineFactoryConfig.java
@@ -7,15 +7,20 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.Message;
+import org.springframework.statemachine.action.Action;
 import org.springframework.statemachine.config.EnableStateMachineFactory;
 import org.springframework.statemachine.config.EnumStateMachineConfigurerAdapter;
 import org.springframework.statemachine.config.builders.StateMachineConfigurationConfigurer;
 import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
 import org.springframework.statemachine.listener.StateMachineListener;
 import org.springframework.statemachine.listener.StateMachineListenerAdapter;
 import org.springframework.statemachine.state.State;
 
+import java.time.Duration;
 import java.util.EnumSet;
+
+import static com.iceservices.poc.workflowmgr.utils.UtilityFunctions.tryToSleep;
 
 
 @Slf4j
@@ -52,6 +57,27 @@ public class StatemachineFactoryConfig extends EnumStateMachineConfigurerAdapter
             public void eventNotAccepted(Message<MasteringEvent> event) {
                 log.info("Event not accepted {}", event);
             }
+        };
+    }
+
+    @Override
+    public void configure(StateMachineTransitionConfigurer<MasteringState, MasteringEvent> transitions) throws Exception {
+        // @formatter:off
+        transitions
+                .withExternal()
+                    .source(MasteringState.INITIAL)
+                    .event(MasteringEvent.SAVE_MASTER_REQUESTED)
+                    .action(saveMasterAction())
+                    .target(MasteringState.SAVED_MASTER_WORK);
+        // @formatter:on
+    }
+
+    @Bean
+    Action<MasteringState, MasteringEvent> saveMasterAction() {
+        return context -> {
+            log.info("...Simulating create a work master with context {}", context);
+            tryToSleep(Duration.ofSeconds(3));
+            log.info("...work will be assumed created for simplicity");
         };
     }
 }
